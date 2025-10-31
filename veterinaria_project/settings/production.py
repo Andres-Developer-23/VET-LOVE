489f0722ddbe26e8fb675bd055af41fca3ce06b2
Andres_Bravo <andres01032001@gmail.com>
2025-10-31 01:04:29 -0500

    Final fix: DEBUG=False and ALLOWED_HOSTS=['*'] for production

--- veterinaria_project/settings/production.py
+++ veterinaria_project/settings/production.py
@@ -5,14 +5,10 @@ import dj_database_url
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
-# Production allowed hosts
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# Production allowed hosts - Allow all hosts for Render deployment
+ALLOWED_HOSTS = ['*']
 
 
 # Security settings for production
