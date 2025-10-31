3fd05ab04973e5b29f683855f95f65b25571195a
Andres_Bravo <andres01032001@gmail.com>
2025-10-31 00:21:30 -0500

    Enable DEBUG=True temporarily for Render deployment troubleshooting

--- veterinaria_project/settings/production.py
+++ veterinaria_project/settings/production.py
@@ -5,7 +5,7 @@ import dj_database_url
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Production allowed hosts - Allow all hosts for Render deployment
 ALLOWED_HOSTS = ['*']
